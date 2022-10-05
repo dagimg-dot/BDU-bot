72c49e7c2729f1d2068b87db0c48443898c5d42a
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-05 18:29:44 +0300

    Update messages_user.py

--- handlers/messages_user.py
+++ handlers/messages_user.py
@@ -2,11 +2,9 @@ from telebot import TeleBot
 from telebot.types import Message
 from util.useful_lists import *
 from util.keyboard_buttons import buttons
-from util.validate_login import *
 from handlers.back_button_handler import back_button_handler
 from functionalities.SIMS.bdu_sims_login import login
 from functionalities.GPA_Prediction.gpa_prediction_menu import predict_gpa
-
 
 
 def main_messages(message: Message, bot: TeleBot):
@@ -14,17 +12,10 @@ def main_messages(message: Message, bot: TeleBot):
         bot.send_message(message.from_user.id,
                              "I didn't get what you say ...")
     elif message.text == menu[0]:
-        # login(message)
         login(message)
-        # bot.send_message(message.chat.id," To Login",reply_markup=buttons("s_login"))
     elif message.text == menu[1]:
-        # markup = telebot.types.ReplyKeyboardRemove()
-        # gpa_calculator.gpa_prediction(message)
         predict_gpa(message)
-        # bot.send_message(message.chat.id,"Predict GPA")
-        # msg = "This is a GPA Prediction Tool. To use it please enter your department . . ."
-        # bot.send_message(message.from_user.id, msg, reply_markup=markup)
-    elif master_check == True:
+    elif master_check[len(master_check)-1] == '1':
         if message.text == success_login[0]:
             buttons_clicked.append(message.text)
             bot.send_message(message.from_user.id, success_login[0],
@@ -66,9 +57,9 @@ def main_messages(message: Message, bot: TeleBot):
                                    reply_markup=buttons("my_status"))
             bot.register_next_step_handler(msg, sgrade_year_handler)
         elif message.text == "Back":
-            back_button_handler(message,bot)
+            back_button_handler(message)
         elif message.text == "Back to Menu":
             still_loggedin_checker(message)
-    elif master_check == False:
+    else:
         bot.send_message(
             message.chat.id, "You are not logged in yet , please press Login button and continue.")
