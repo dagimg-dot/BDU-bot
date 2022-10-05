f6eea798b729e6b29db99eb4f384976790204053
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-05 23:03:48 +0300

    added grade list for gpa calculation

--- util/useful_lists.py
+++ util/useful_lists.py
@@ -8,10 +8,16 @@ MyS = ["Cumulative GPA - CGPA", "Semester GPA - SGPA", "Semester Grades"]
 
 Buttons = menu + success_login + MyC + MyS + ["Back", "Back to Menu"]
 
+Grades = ['A+','a+','A','a','A-','a-','B+','b+','B','b','B-','b-','C+','c+','C','c','C-','c-','D','d','F','f']
+
 buttons_clicked = []
 
 master_check = ['0']
 
+length = len(master_check)
+if length != 1:
+    for i in master_check[:length-1]:
+        master_check.remove(i)
 
 cardinal_ordinal = {
     1: 'first',
@@ -23,9 +29,24 @@ cardinal_ordinal = {
     7: 'seventh',
 }
 
+grade_list = {
+    "A+": 4,
+    "A": 4,
+    "A-": 3.75,
+    "B+": 3.5,
+    "B": 3,
+    "B-": 2.75,
+    "C+": 2.5,
+    "C": 2,
+    "C-": 1.75,
+    "D": 1,
+    "F": 0
+}
+
 with open('util\Dept\dept_list.json') as f:
     data = json.load(f)
 
 dept_title =  []
 for i in range(len(data)):
     dept_title.append(data[i]['Department Title'])
+
