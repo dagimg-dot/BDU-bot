80b5925cefaa502e7dd9a0210200c3fbad41c7a1
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-11 22:34:30 +0300

    fixed a bug in call back query handler

--- functionalities/GPA_Prediction/gpa_prediction_menu.py
+++ functionalities/GPA_Prediction/gpa_prediction_menu.py
@@ -1,6 +1,5 @@
 from bot import bot
 from telebot import types
-import json
 from functionalities.GPA_Prediction.additional_info import get_add_info
 from util.useful_lists import dept_title
 
@@ -10,15 +9,14 @@ def predict_gpa(message):
     markup.row_width = 1
     dept_buttons = [types.InlineKeyboardButton(x,callback_data=x) for x in dept_title]
     markup.add(*dept_buttons)
-    bot.send_message(
-        message.chat.id, 'Choose Department',reply_markup=markup)
+    bot.send_message(message.chat.id, 'Choose Department',reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda call:True)
-def user_answer_dept(call):
-    message = call.message
+@bot.callback_query_handler(lambda query: query.data in dept_title)
+def user_answer_dept(query):
+    message = query.message
     for i in range(len(dept_title)):
-        if call.data == dept_title[i]:
+        if query.data == dept_title[i]:
             msg = dept_title[i]
-            bot.answer_callback_query(call.id,"You Chose "+msg)
+            bot.answer_callback_query(query.id,"You Chose "+msg)
             get_add_info(message,msg)
 
