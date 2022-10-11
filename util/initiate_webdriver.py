c3403ea85adfdee1904e4382fb46531f1ae8c68a
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-11 22:38:44 +0300

    Update initiate_webdriver.py

--- util/initiate_webdriver.py
+++ util/initiate_webdriver.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
+
 
 
 def initiate_driver():
