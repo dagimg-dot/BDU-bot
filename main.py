95a2ad4b0f241824c58849f7e8eeddec231e63d5
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-11 22:37:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from handlers.commands_user import start_message,help_message,menu_handler
-from handlers.messages_user import main_messages
+from handlers.main_menu import main_menu_messages
+from util.useful_lists import *
 from bot import bot
 
 
@@ -11,10 +12,9 @@ def command_handlers():
     bot.register_message_handler(help_message, commands=['help'], pass_bot=True)
     bot.register_message_handler(menu_handler, commands=['menu'], pass_bot=True)
     
-# Handles all incoming button clicks
+# Handles all incoming button clicks and messages
 def message_handlers():
-    bot.register_message_handler(main_messages,pass_bot=True)
-
+    bot.register_message_handler(main_menu_messages,pass_bot=True)
 
 command_handlers()
 
@@ -23,6 +23,5 @@ message_handlers()
 
 def run():
     bot.infinity_polling()
-
 
 run()
