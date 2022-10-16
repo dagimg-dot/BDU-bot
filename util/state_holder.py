adb5191346cf6b9ab4f026d6eba73462c6150e07
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-16 20:59:23 +0300

    Update state_holder.py

--- util/state_holder.py
+++ util/state_holder.py
@@ -4,11 +4,11 @@ from util.user_database import users
 def state_changer(message,page):
     for key,value in users[message.from_user.id].get_state().items():
         if key == page:
-            users[message.from_user.id].set_state(key,True)
+            users[message.from_user.id].set_state(key,users[message.from_user.id].get_state()[key]+1)
         else:
-            users[message.from_user.id].set_state(key,False)
+            users[message.from_user.id].set_state(key,0)
 
 
 def state_reset(message):
     for key,value in users[message.from_user.id].get_state().items():
-        users[message.from_user.id].set_state(key,False)
+        users[message.from_user.id].set_state(key,0)
