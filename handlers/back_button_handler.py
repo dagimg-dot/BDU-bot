0de14ea1ef178f205719543cfb7882c8d6f9ba00
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-28 18:14:51 +0300

    Update back_button_handler.py
    multiple user

--- handlers/back_button_handler.py
+++ handlers/back_button_handler.py
@@ -7,14 +7,14 @@ from util.user_database import users
 
 
 def still_loggedin_checker(message):
-    global back 
+    users[message.from_user.id].message_id[0] = message.message_id
     back = message
     markup = types.InlineKeyboardMarkup()
     yes_btn = types.InlineKeyboardButton('Yes', callback_data="Yes")
     no_btn = types.InlineKeyboardButton('No', callback_data="No")
     markup.add(yes_btn, no_btn)
-    global sent_msg 
     sent_msg = bot.send_message(message.chat.id, 'You are still Logged in. Do you want to log out ?', reply_markup=markup)
+    users[message.from_user.id].message_id[1] = sent_msg.message_id
 
 @bot.callback_query_handler(lambda query: query.data in ["Yes","No"])
 def user_answer_logout(query):
@@ -24,12 +24,12 @@ def user_answer_logout(query):
         msg = "You are logged out !!"
         users[query.from_user.id].is_logged_in = False
         users[query.from_user.id].is_driver_opened = False
-        user_state_reset(back)
+        user_state_reset(query)
         bot.answer_callback_query(query.id,msg,show_alert=True)
-        cleaner(sent_msg)
-        cleaner(back)
+        bot.delete_message(query.from_user.id,users[query.from_user.id].message_id[1])
+        bot.delete_message(query.from_user.id,users[query.from_user.id].message_id[0])
         bot.send_message(query.from_user.id, msg, reply_markup=buttons("Menu"))
     elif query.data == "No":
         bot.answer_callback_query(query.id,"Logging out Cancelled",show_alert=True)
-        cleaner(sent_msg)
-        cleaner(back)
+        bot.delete_message(query.from_user.id,users[query.from_user.id].message_id[1])
+        bot.delete_message(query.from_user.id,users[query.from_user.id].message_id[0])
