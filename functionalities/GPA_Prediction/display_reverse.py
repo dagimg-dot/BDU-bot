10c9f5784699b2f6d709c45f1ac100f9f39939b6
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-28 15:42:45 +0300

    fixed bug related to grade display
    for multiple users

--- functionalities/GPA_Prediction/display_reverse.py
+++ functionalities/GPA_Prediction/display_reverse.py
@@ -25,7 +25,9 @@ def display_final(message):
     full_str = str(users[message.from_user.id].current_state + 1) + " out of 5" + "  GPA:  " + str(round(users[message.from_user.id].gpa_list[users[message.from_user.id].current_state],3)) + "\n\n" + "\n".join("{}  {}".format(v, k)
                     for k, v in users[message.from_user.id].possibility_dict[users[message.from_user.id].current_state].items())
     main_msg = bot.send_message(message.from_user.id,full_str,reply_markup=InlineKeyboardMarkup().row(prev_button,next_button))
-    users[message.from_user.id].rev_message_id = main_msg.message_id
+    users[message.from_user.id].message_id[2] = main_msg.message_id
+    print(users[message.from_user.id].message_id[2]) 
+
 
 
 @bot.callback_query_handler(lambda query: query.data in ["p","n"])
@@ -41,7 +43,7 @@ def rotate_prediction(query):
             next_button = InlineKeyboardButton("➡️ Next", callback_data="n")
             full_str = str(users[query.from_user.id].current_state + 1) + " out of 5" + "  GPA:  " + str(round(users[query.from_user.id].gpa_list[users[query.from_user.id].current_state],3)) + "\n\n" + "\n".join("{}  {}".format(v, k)
                 for k, v in users[query.from_user.id].possibility_dict[users[query.from_user.id].current_state].items())
-            bot.edit_message_text(full_str,users[query.from_user.id].user_id,users[query.from_user.id].rev_message_id,reply_markup=InlineKeyboardMarkup().row(prev_button,next_button))
+            bot.edit_message_text(full_str,users[query.from_user.id].user_id,users[query.from_user.id].message_id[2],reply_markup=InlineKeyboardMarkup().row(prev_button,next_button))
             print(users[query.from_user.id].get_rev_state_full())  
     elif query.data == "n":
         if users[query.from_user.id].current_state == 4:
@@ -53,5 +55,5 @@ def rotate_prediction(query):
             next_button = InlineKeyboardButton("➡️ Next", callback_data="n")
             full_str = str(users[query.from_user.id].current_state + 1) + " out of 5" + "  GPA:  " + str(round(users[query.from_user.id].gpa_list[users[query.from_user.id].current_state],3)) + "\n\n" + "\n".join("{}  {}".format(v, k)
                 for k, v in users[query.from_user.id].possibility_dict[users[query.from_user.id].current_state].items())
-            bot.edit_message_text(full_str,users[query.from_user.id].user_id,users[query.from_user.id].rev_message_id,reply_markup=InlineKeyboardMarkup().row(prev_button,next_button))
+            bot.edit_message_text(full_str,users[query.from_user.id].user_id,users[query.from_user.id].message_id[2],reply_markup=InlineKeyboardMarkup().row(prev_button,next_button))
             print(users[query.from_user.id].get_rev_state_full())  
