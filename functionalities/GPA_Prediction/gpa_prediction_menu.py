b08445dc95788bce3c8961de136ab9dac2cf5710
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-28 18:14:24 +0300

    Update gpa_prediction_menu.py
    multiple user

--- functionalities/GPA_Prediction/gpa_prediction_menu.py
+++ functionalities/GPA_Prediction/gpa_prediction_menu.py
@@ -3,6 +3,7 @@ from telebot import types
 from functionalities.GPA_Prediction.additional_info import get_add_info
 from util.message_cleaner import cleaner
 from util.useful_lists import dept_title
+from util.user_database import users
 
 
 def predict_gpa(message):
@@ -10,16 +11,15 @@ def predict_gpa(message):
     markup.row_width = 1
     dept_buttons = [types.InlineKeyboardButton(x,callback_data=x) for x in dept_title]
     markup.add(*dept_buttons)
-    global sent_msg 
     sent_msg = bot.send_message(message.chat.id, 'Choose Department',reply_markup=markup)
+    users[message.from_user.id].message_id[3] = sent_msg.message_id
 
 @bot.callback_query_handler(lambda query: query.data in dept_title)
 def user_answer_dept(query):
-    message = query.message
     for i in range(len(dept_title)):
         if query.data == dept_title[i]:
             msg = dept_title[i]
             bot.answer_callback_query(query.id,"You Chose "+msg)
-            cleaner(sent_msg)
-            get_add_info(message,msg)
+            bot.delete_message(users[query.from_user.id].user_id,users[query.from_user.id].message_id[3])
+            get_add_info(query,msg)
 
