0c89a774721ab7bb10a6da8b88d5489f59413dd0
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-10-28 20:09:58 +0300

    Update useful_lists.py

--- util/useful_lists.py
+++ util/useful_lists.py
@@ -22,6 +22,16 @@ cardinal_ordinal = {
     7: 'seventh',
 }
 
+cardinal_roman = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+}
+
 grade_list = {
     "A+": 4,
     "A": 4,
