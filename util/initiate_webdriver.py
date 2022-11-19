f86ec7cc982d477b2f05dd71b31a6a2c2e5b12f2
Dagim G. Astatkie <workflow.jd@gmail.com>
2022-11-20 00:50:07 +0300

    Update initiate_webdriver.py

--- util/initiate_webdriver.py
+++ util/initiate_webdriver.py
@@ -4,5 +4,5 @@ from selenium.webdriver.chrome.options import Options
 def initiate_driver():
     options = Options()
     options.headless = True
-    page_to_scrape = webdriver.Chrome(options=options)
+    page_to_scrape = webdriver.Chrome(executable_path='./chromedriver.exe', port=8000, options=options)
     return page_to_scrape
